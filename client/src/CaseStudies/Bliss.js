eaa9be18dbb7d83e114bad71222558e2f69a1a8b
mvcloud <114954862+mvcloud@users.noreply.github.com>
2023-07-06 09:42:42 -0500

    Add files via upload

---
+++ client/src/CaseStudies/Bliss.js
@@ -0,0 +1,16 @@
+const Bliss = () => {
+    const box = {
+        color: "pink",
+        backgroundColor: "lavenderblush",
+        width: "33.33%",
+        height: 500,
+        display: "inline-flex",
+        
+    }
+
+    return (
+        <p style={box}>bliss</p>
+    )
+}
+
+export default Bliss;
