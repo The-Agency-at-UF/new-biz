dc521eac30221efb03fc5e01f9dd3207c961fb08
mvcloud <114954862+mvcloud@users.noreply.github.com>
2023-07-06 09:47:09 -0500

    Update index.js to have website paths

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom';
 import './index.css';
 import App from './App';
+import Brands from './Brands'
+
+//different paths of website
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path:'brands',
+    element: <Brands />
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
