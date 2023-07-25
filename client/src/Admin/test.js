c836dcca2c9945e0d134610df451e4d747ba4c57
Taise M <56640569+tmiyazumi@users.noreply.github.com>
2023-07-25 18:22:06 -0400

    adding the client pages and images

---
+++ client/src/Admin/test.js
@@ -0,0 +1,22 @@
+import {React, useState, useEffect} from 'react';
+import Home from '../Dropdown/home.js';
+import Ultra from '../Pages/Ultra.js';
+
+const API_BASE = "http://localhost:3001";
+
+
+const Test = ({ loggedIn }) => {
+
+    if (loggedIn){
+        return (
+            <Home />
+        )
+    }
+    else{
+        return (
+            <Ultra />
+        )
+    }
+}
+
+export default Test;
