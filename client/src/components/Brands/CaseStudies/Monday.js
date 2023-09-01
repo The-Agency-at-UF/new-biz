cee8da2b45f717c5c3fbe05256eb1070ca04367f
mvcloud <114954862+mvcloud@users.noreply.github.com>
2023-09-01 08:30:59 -0500

    Update Monday.js

--- client/src/components/Brands/CaseStudies/Monday.js
+++ client/src/components/Brands/CaseStudies/Monday.js
@@ -2,9 +2,11 @@ const Monday = () => {
     const box = {
         color: "mediumpurple",
         backgroundColor: "lavender",
-        width: "33.33%",
+        width: "100%",
         height: 500,
-        display: "inline-flex",
+        margin: "auto",
+        display: "block",
+        boxSizing: "border-box"
     }
 
     return (
