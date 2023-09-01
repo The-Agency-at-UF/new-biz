8e9e76fafad5a67369105cec5d3496744ab6a87c
mvcloud <114954862+mvcloud@users.noreply.github.com>
2023-09-01 08:31:34 -0500

    Update Uber.js

--- client/src/components/Brands/CaseStudies/Uber.js
+++ client/src/components/Brands/CaseStudies/Uber.js
@@ -2,9 +2,11 @@ const Uber = () => {
     const box = {
         color: "green",
         backgroundColor: "darkseagreen",
-        width: "33.33%",
+        width: "100%",
         height: 500,
-        display: "inline-flex",
+        margin: "auto",
+        display: "block",
+        boxSizing: "border-box"
     }
 
     return (
