7f0284e6a3c94df55040350544f1fa84aad126cd
mvcloud <114954862+mvcloud@users.noreply.github.com>
2023-10-08 10:35:02 -0400

    Update Home.js to include banner

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,14 @@
 // This is the main component we will add all other components to the site here
 import React from "react";
 import "./Home.css";
+import Banner from '../components/Banner/Banner';
 
 const Home = () => {
-  return <></>;
+  return (
+    <div>
+      <Banner />
+    </div>
+  );
 };
 
 export default Home;
