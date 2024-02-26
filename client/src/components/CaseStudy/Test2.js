65cb569f18eb648c4d0bd58d6366b9156d8e4537
Taise M <56640569+tmiyazumi@users.noreply.github.com>
2024-02-26 13:41:25 -0500

    pushing url changes and firebase changes

---
+++ client/src/components/CaseStudy/Test2.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { useParams } from 'react-router-dom';
+
+
+const Test2 = () => {
+
+
+    return (
+        <div>
+        <h1>Test2</h1>
+        </div>
+
+    );
+}
+
+export default Test2;
---
+++ client/src/components/CaseStudy/Test3.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { useParams } from 'react-router-dom';
+
+
+const Test3 = () => {
+
+
+    return (
+        <div>
+        <h1>Test3</h1>
+        </div>
+
+    );
+}
+
+export default Test3;
