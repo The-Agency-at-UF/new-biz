95488688c7d33d3da0f8ab35904b1ed2c3d6fbcc
“ashguerra” <ashguerraschool@gmail.com>
2024-03-15 01:41:10 -0400

    progress bar

--- client/src/components/ProgressBar/ProgressBar.js
+++ client/src/components/ProgressBar/ProgressBar.js
@@ -4,7 +4,17 @@ import "./ProgressBar.css";
 // examples used:
 // Scroll Bar: https://codepen.io/DevSkyler/details/QWqOdmp
 
-const ProgressBar = () => {
+const ProgressBar = ({ highlight }) => {
+  const sections = [
+    "Home",
+    "About",
+    "Meet The Team",
+    "Our Clients",
+    "Case 1",
+    "Case 2",
+    "Case 3",
+  ];
+
   // so refresh doesn't reset the scroll bar
   useEffect(() => {
     const scroll = document.documentElement.scrollTop;
@@ -14,7 +24,7 @@ const ProgressBar = () => {
     let scrolled = (scroll / height) * 100;
     if (scrolled >= 0) {
       document.getElementById("progressbar").style.height =
-        100 - scrolled + "%";
+        scrolled + "%";
     }
   }, []);
 
@@ -30,14 +40,33 @@ const ProgressBar = () => {
         document.documentElement.clientHeight;
       let scrolled = (scroll / height) * 100;
       if (scrolled >= 0) {
-        progressBar.style.height = 100 - scrolled + "%";
+        progressBar.style.height = scrolled + "%";
       }
     };
   });
 
   return (
-    <div className="progress-container">
-      <div id="progressbar" className="progress-bar"></div>
+    <div className="other-container">
+        <div className="progress-container">
+              {/* <div className="sections">
+                {sections.map((buttonName, index) => {
+                  if (Number(highlight.substring(7)) === index + 1) {
+                    return (
+                      <div
+                        className={`nav-button nav-button-highlighted nav-${index + 1}`}
+                      >
+                        {buttonName}
+                      </div>
+                    );
+                  } else {
+                    return (
+                      <div className={`nav-button nav-${index + 1}`}>{buttonName}</div>
+                    );
+                  }
+                })}
+              </div> */}
+              <div id="progressbar" className="progress-bar"></div>
+      </div>
     </div>
   );
 };
